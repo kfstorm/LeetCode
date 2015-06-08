051e7b86d89792006fc315543952dd79aa2ad330
Kai Yang <yk000123@sina.com>
2015-06-08 13:54:47 +0800

    223. Rectangle Area

---
+++ 223.RectangleArea.Test.cs
@@ -0,0 +1,12 @@
+﻿using NUnit.Framework;
+
+[TestFixture]
+public class RectangleAreaTest
+{
+    [TestCase(-3, 0, 3, 4, 0, -1, 9, 2, 45)]
+    public void Test(int a, int b, int c, int d, int e, int f, int g, int h, int expectedArea)
+    {
+        var result = new Solution().ComputeArea(a, b, c, d, e, f, g, h);
+        Assert.AreEqual(expectedArea, result);
+    }
+}
