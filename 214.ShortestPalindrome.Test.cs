62ba539e2d902ba40a2958001486ebf7929f82be
Kai Yang <yk000123@sina.com>
2015-06-08 14:29:46 +0800

    214. Shortest Palindrome

---
+++ 214.ShortestPalindrome.Test.cs
@@ -0,0 +1,14 @@
+﻿using NUnit.Framework;
+
+[TestFixture]
+public class ShortestPalindromeTest
+{
+    [TestCase("", "")]
+    [TestCase("aacecaaa", "aaacecaaa")]
+    [TestCase("abcd", "dcbabcd")]
+    public void Test(string s, string expectedPalindrome)
+    {
+        var answer = new Solution().ShortestPalindrome(s);
+        Assert.AreEqual(expectedPalindrome, answer);
+    }
+}
