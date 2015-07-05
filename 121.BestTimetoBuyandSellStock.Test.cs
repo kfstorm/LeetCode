a31f4ca89c378aaf535a044aa74d474ecc84e02f
Kai Yang <yk000123@sina.com>
2015-07-05 19:07:11 +0800

    121. Best Time to Buy and Sell Stock

---
+++ 121.BestTimetoBuyandSellStock.Test.cs
@@ -0,0 +1,17 @@
+using System;
+using NUnit.Framework;
+
+[TestFixture]
+public class TestClass
+{
+    [TestCase("1 2 3 4 5", 4)]
+    [TestCase("5 4 3 2 1", 0)]
+    [TestCase("1 2 5 3 4", 4)]
+    [TestCase("2 5 3 4 1", 3)]
+    public void TestMethod(string pricesString, int expectedResult)
+    {
+        var prices = Array.ConvertAll(pricesString.Split(), int.Parse);
+        var result = new Solution().MaxProfit(prices);
+        Assert.AreEqual(expectedResult, result);
+    }
+}
