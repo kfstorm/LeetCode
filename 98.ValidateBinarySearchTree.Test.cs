1a9103205d69d06a79049843bd7763a64eb63eb6
Kai Yang <yk000123@sina.com>
2015-07-19 18:05:18 +0800

    98. Validate Binary Search Tree

---
+++ 98.ValidateBinarySearchTree.Test.cs
@@ -0,0 +1,38 @@
+using Newtonsoft.Json;
+using NUnit.Framework;
+
+/**
+ * Definition for a binary tree node.
+ */
+public class TreeNode {
+    public int val;
+    public TreeNode left;
+    public TreeNode right;
+    public TreeNode(int x) { val = x; }
+}
+
+[TestFixture]
+public class TestClass
+{
+    [TestCase("null", true)]
+    [TestCase("{ val: 1 }", true)]
+    [TestCase("{ val: 1, left: { val: 2 }, right: { val: 3 } }", false)]
+    [TestCase("{ val: 2, left: { val: 1 }, right: { val: 3 } }", true)]
+    [TestCase("{ val: 2, left: { val: 1 }, right: { val: 3, left: { val: 4 }, right: { val: 5 } } }", false)]
+    [TestCase("{ val: 2, left: { val: 1 }, right: { val: 3, left: { val: 1 }, right: { val: 5 } } }", false)]
+    [TestCase("{ val: 2, left: { val: 1 }, right: { val: 4, left: { val: 3 }, right: { val: 5 } } }", true)]
+    [TestCase("{ val: -2147483648 }", true)]
+    [TestCase("{ val: -2147483648, left: { val: 2 }, right: { val: 3 } }", false)]
+    [TestCase("{ val: -2147483648, left: { val: -2147483648 }, right: { val: 2147483647 } }", false)]
+    [TestCase("{ val: -2147483647, left: { val: -2147483648 }, right: { val: 2147483647 } }", true)]
+    [TestCase("{ val: 2147483647 }", true)]
+    [TestCase("{ val: 2147483647, left: { val: 2 }, right: { val: 3 } }", false)]
+    [TestCase("{ val: 2147483647, left: { val: -2147483648 }, right: { val: 2147483647 } }", false)]
+    [TestCase("{ val: 2147483646, left: { val: -2147483648 }, right: { val: 2147483647 } }", true)]
+    public void TestMethod(string rootString, bool expectedResult)
+    {
+        var root = JsonConvert.DeserializeObject<TreeNode>(rootString);
+        var answer = new Solution().IsValidBST(root);
+        Assert.AreEqual(expectedResult, answer);
+    }
+}
