92613c88cae8ebd4cda033d3e2fab43781d8f32f
Kai Yang <yk000123@sina.com>
2015-08-11 20:24:31 +0800

    133. Clone Graph

---
+++ 133.CloneGraph.Test.cs
@@ -0,0 +1,38 @@
+using System.Collections.Generic;
+using System.Linq;
+using Newtonsoft.Json;
+using NUnit.Framework;
+
+public class UndirectedGraphNode {
+    public int label;
+    public IList<UndirectedGraphNode> neighbors;
+    public UndirectedGraphNode(int x) { label = x; neighbors = new List<UndirectedGraphNode>(); }
+};
+
+
+[TestFixture]
+public class TestClass
+{
+    [Test]
+    public void TestMethod()
+    {
+        var nodes = Enumerable.Range(1, 5).Select(i => new UndirectedGraphNode(i)).ToList();
+        for (var i = 0; i < nodes.Count; ++i)
+        {
+            for (var j = 0; j < nodes.Count; ++j)
+            {
+                if (i != j)
+                {
+                    nodes[i].neighbors.Add(nodes[j]);
+                }
+            }
+        }
+
+        var result = new Solution().CloneGraph(nodes[0]);
+        var settings = new JsonSerializerSettings
+        {
+            PreserveReferencesHandling = PreserveReferencesHandling.Objects,
+        };
+        Assert.AreEqual(JsonConvert.SerializeObject(nodes[0], settings), JsonConvert.SerializeObject(result, settings));
+    }
+}
