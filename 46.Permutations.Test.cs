22f9d6722f59d4c514b098119b146070584b3b62
Kai Yang <yk000123@sina.com>
2015-09-01 21:06:20 +0800

    46. Permutations

---
+++ 46.Permutations.Test.cs
@@ -0,0 +1,16 @@
+using Newtonsoft.Json;
+using NUnit.Framework;
+
+[TestFixture]
+public class TestClass : TestClassBase
+{
+    [TestCase("[1,2,3]", "[[1,2,3],[1,3,2],[2,1,3],[2,3,1],[3,1,2],[3,2,1]]")]
+    [TestCase("[]", "[]")]
+    [TestCase("[1]", "[[1]]")]
+    public void TestMethod(string numsString, string expectedResultString)
+    {
+        var nums = JsonConvert.DeserializeObject<int[]>(numsString);
+        var result = new Solution().Permute(nums);
+        Assert.AreEqual(expectedResultString, JsonConvert.SerializeObject(result));
+    }
+}
