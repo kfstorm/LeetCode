afec845efd6dae3ac8aa17cd842ae537fc3ab8d1
Kai Yang <kfstorm@outlook.com>
2020-01-26 17:54:50 +0800

    Update code due to update of test cases and interfaces

--- shared/UndirectedGraphNode.cs
+++
@@ -1,7 +0,0 @@
-using System.Collections.Generic;
-
-public class UndirectedGraphNode {
-    public int label;
-    public IList<UndirectedGraphNode> neighbors;
-    public UndirectedGraphNode(int x) { label = x; neighbors = new List<UndirectedGraphNode>(); }
-};
